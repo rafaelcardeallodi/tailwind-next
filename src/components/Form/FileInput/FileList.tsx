b46b596037ba610bf9995e67f40867baaad75cc9
Rafael Lodi <rafaelcardeallodi@hotmail>
2023-08-25 23:00:15 -0300

    Criando efeitos de foco

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -2,13 +2,16 @@
 
 import { Trash2, UploadCloud } from 'lucide-react'
 import { useFileInput } from './Root'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { formatBytes } from '@/app/utils/format-bytes'
 
 export function FileList() {
   const { files } = useFileInput()
 
+  const [parent] = useAutoAnimate()
+
   return (
-    <div className="mt-4 space-y-3">
+    <div ref={parent} className="mt-4 space-y-3">
       {files.map((file) => {
         return (
           <div
--- src/components/Form/Input/Control.tsx
+++ src/components/Form/Input/Control.tsx
@@ -5,7 +5,7 @@ type ControlProps = ComponentProps<'input'>
 export function Control(props: ControlProps) {
   return (
     <input
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
       {...props}
     />
   )
